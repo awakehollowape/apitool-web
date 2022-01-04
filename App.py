b7e47835f6e7f6557e7c343f223031eb0b8db866
Jose Jose <awakehollowape@gmail.com>
2022-01-04 20:25:43 -0300

    finished beta of main page, and started with login and register page (only css and html)

--- App.py
+++ App.py
@@ -1,10 +1,17 @@
 import os
-from flask import Flask
+from flask import Flask, flash
 from flask import render_template
-from flask import send_from_directory
+from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"] = "databases.000webhost.com"
+app.config["MYSQL_USER"] = "id18157936_apitool"
+app.config["MYSQL_PASSWORD"] = "8$v<(WUBOEfc}^r9"
+app.config["MYSQL_DB"] = "id18157936_test"
+
+mysql = MySQL(app)
+
  
 @app.route('/')
 def Index():
@@ -20,7 +27,7 @@ def contact():
 
 @app.route('/login')
 def login():
-    return 'login' 
+    return render_template('login.html') 
 
 
 if __name__ == "__main__":
