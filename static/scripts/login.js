9fe2029b52058bc7a3f43321f2aae6e702442be1
Jose Jose <awakehollowape@gmail.com>
2022-01-05 16:03:39 -0300

    added login - register page with some js script

---
+++ static/scripts/login.js
@@ -0,0 +1,12 @@
+selectLogin = function(e) {
+    document.getElementById("register").style.display = "none";
+    document.getElementById("login").style = "display: block";
+}
+selectRegister = function(e) {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("register").style = "display: block";
+}
+selectRegisterStepTwo = function(e) {
+    document.getElementById("register").style.display = "none";
+    document.getElementById("registerStepTwo").style = "display: block";
+}
